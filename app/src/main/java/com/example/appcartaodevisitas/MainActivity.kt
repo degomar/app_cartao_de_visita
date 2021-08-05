ff6fe2bb73d50ff905bde2d84c2ed8ebf2a52e66
Eric <eric.degomar@gmail.com>
2021-08-05 11:53:22 -0300

    adicionado activity de incluir cartoes e intent na mainactiivity

--- app/src/main/java/com/example/appcartaodevisitas/MainActivity.kt
+++ app/src/main/java/com/example/appcartaodevisitas/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.appcartaodevisitas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.appcartaodevisitas.databinding.ActivityMainBinding
+import com.example.appcartaodevisitas.ui.AddBusinessCardActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.btnAddCard.setOnClickListener {
+           val addIntent = Intent(this, AddBusinessCardActivity::class.java)
+            startActivity(addIntent)
+        }
+
     }
+
+
+
+
 }
+
